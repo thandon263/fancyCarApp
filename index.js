c7740a28d4f8fff7170b336b6fb57df49ab342a0
Thando <ncubethando20@gmail.com>
2018-08-17 22:29:50 -0400

    Implemented the React - bundled the application.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var express = require("express");
 var app = express();
 
 // [ PUBLIC ] - For static assets
-app.use(express.static('public'));
+app.use(express.static(__dirname, 'public'));
+app.use(express.static(__dirname, 'build'));
 
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+
+    // [BASE CONFIGURATION]
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        'react',
+                        'stage-0',
+                        ['env', { targets: { browsers: ['last 2 versions'] }}]
+                    ]
+                }
+            }
+        ]
+    },
+    devtool: 'source-map'
+}
