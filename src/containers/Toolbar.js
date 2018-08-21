0faa22ac526b13930632b05a99c46a4101b910d7
Thando <ncubethando20@gmail.com>
2018-08-21 04:44:13 -0400

    Refactored the Toolbar code for presentation

--- src/containers/Toolbar.js
+++ src/containers/Toolbar.js
@@ -4,9 +4,21 @@ class Toolbar extends React.Component {
     render() {
         return(
             <div className="toolbar">
-                <div className="toolbar--name" onClick={this.props.all}>all</div>
-                <div className="toolbar--name" onClick={this.props.nameSort}>Name</div>
-                <div className="toolbar--availability" onClick={this.props.availability}>Availability</div>
+                <div 
+                    className="toolbar--name" 
+                    onClick={this.props.all}>
+                    all
+                </div>
+                <div 
+                    className="toolbar--name" 
+                    onClick={this.props.nameSort}>
+                    Name
+                </div>
+                <div 
+                    className="toolbar--availability" 
+                    onClick={this.props.availability}>
+                    Availability
+                </div>
             </div>
         );
     }
